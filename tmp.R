7455b36ea90255a23396b5b298c5dda9caaaf459
XSun <sxt1229@uchicago.edu>
2023-06-10 10:29:01 -0500

    update

--- tmp.R
+++ tmp.R
@@ -1,26 +1,17 @@
-all <- readRDS("/project2/guiming/xsun/1.brain_wingo_latest/brain_output/brain_ctwas_gene_res.RDS")
-all_08 <- all[all$susie_pip>0.8,]
+[data statistics -- brain, Wingo](https://xsun1229.github.io/proteome_alzheimer/data_stat_brain_wingo.html)
 
-subset <- readRDS("/project2/guiming/xsun/1.brain_wingo_latest_subset/brain_output/brain_ctwas_gene_res.RDS")
-subset_08 <- subset[subset$susie_pip>0.8,]
+[Results for GCST90012877_buildGRCh37](https://xsun1229.github.io/proteome_alzheimer/brain_877_ldukbb.html)
 
-sum(all_08$id %in% subset_08$id)
+[Results for GCST90012877_buildGRCh37 -- latest ctwas package, LD REF: UKBB](https://xsun1229.github.io/proteome_alzheimer/brain_877_latest.html)
 
+[Results for GCST90012877_buildGRCh37 -- latest ctwas package, LD REF: 1000G](https://xsun1229.github.io/proteome_alzheimer/brain_877_latest_ld1kg.html)
 
-> subset <- readRDS("/project2/guiming/xsun/1.brain_wingo_latest_subset/brain_output/brain_ctwas_gene_res.RDS")
-> all <- readRDS("/project2/guiming/xsun/1.brain_wingo_latest/brain_output/brain_ctwas_gene_res.RDS")
-> all_08 <- all[all$susie_pip>0.8,]
-> subset_08 <- subset[subset$susie_pip>0.8,]
-> sum(all_08$id %in% subset_08$id)
-[1] 42
-> View(subset_08)
-> View(all_08)
-> a <- subset_08[subset_08$id %in% all_08$id,]
-> b <- merge(a,all_08,by="id")
-> plot(b$susie_pip.x,b$susie_pip.y)
-Fontconfig warning: ignoring English_United States.1252: not a valid language tag
-> plot(b$z.x,b$z.y)
-> c <- merge(all,subset_08,by="id")
-> plot(c$z.x,c$z.y)
-> plot(c$susie_pip.x,c$susie_pip.y)
-> plot(all$z, subset$z)
+[Comparing the gene z-score](https://xsun1229.github.io/proteome_alzheimer/compare_ld.html)
+
+[Results for GCST90012877_buildGRCh37 -- latest ctwas package, GWAS: subset, LD REF: UKBB](https://xsun1229.github.io/proteome_alzheimer/brain_877_latest_subset_ukbb.html)
+
+[Results for GCST90012877_buildGRCh37 -- latest ctwas package, GWAS: subset, LD REF: 1000G](https://xsun1229.github.io/proteome_alzheimer/brain_877_latest_subset.html)
+
+[Comparing the gene z-score, GWAS: subset](https://xsun1229.github.io/proteome_alzheimer/compare_ld_subset.html)
+
+[Comparing the results from full GWAS and GWAS subset](https://xsun1229.github.io/proteome_alzheimer/compare_gwas_full_subset_ukbb.html)
