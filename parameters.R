a8f34af34cd25c7f58627e2cf6db3030da327293
XSun <sxt1229@uchicago.edu>
2023-10-26 14:22:14 -0500

    update

--- parameters.R
+++ parameters.R
@@ -11,3 +11,4 @@ legend_side="right"
 legend_panel="cTWAS"
 twas_ymax=NULL
 draw_gene_track=T
+plot_eqtl <- c("ENSG00000176623|wingo_888_brain_expr_lasso_harmo_drop_ambig","ENSG00000176623|wingo_722_brain_protein_lasso_harmo_drop_ambig")
